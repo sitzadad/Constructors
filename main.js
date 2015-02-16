f3e3323c497f26ece41aa9cbf9dd72cbaec64d37
Adam Sitz <sitzadad@Adams-MBP.attlocal.net>
2015-02-16 01:56:19 -0500

    pawns rendering and validating when moved

--- main.js
+++ main.js
@@ -44,32 +44,40 @@ var methods={
 
   },
   renderLight:function(){
-    methods.lightKing = new methods.king({type:'K',color:'white',currentX:'5',currentY:'1'});
-    methods.lightQueen = new methods.queen({type:'Q',color:'white',currentX:'4',currentY:'1'});
-    methods.lightBishopLeft = new methods.bishop({type:'B',color:'white',currentX:'3',currentY:'1'});
-    methods.lightBishopRight = new methods.bishop({type:'B',color:'white',currentX:'6',currentY:'1'});
-    methods.lightRookLeft = new methods.rook({type:'R',color:'white',currentX:'1',currentY:'1'});
-    methods.lightRookRight = new methods.rook({type:'R',color:'white',currentX:'8',currentY:'1'});
-    methods.lightKnightLeft = new methods.knight({type:'N',color:'white',currentX:'2',currentY:'1'});
-    methods.lightKnightRight = new methods.knight({type:'N',color:'white',currentX:'7',currentY:'1'});
-    // tempArray={
-    //   lightPawn1,
-    //   lightPawn2,
-    // }
-    // for(i=0;i<8;i++){
-    //   foo = i+1;
-    //   methods.lightPawn+foo  new methods.pawn({type:'P',color:'white',currentX:foo,currentY:'2'});
-    // }
+    methods.lightKing = new methods.king({type:'K',color:'white',currentX:5,currentY:1});
+    methods.lightQueen = new methods.queen({type:'Q',color:'white',currentX:4,currentY:1});
+    methods.lightBishopLeft = new methods.bishop({type:'B',color:'white',currentX:3,currentY:1});
+    methods.lightBishopRight = new methods.bishop({type:'B',color:'white',currentX:6,currentY:1});
+    methods.lightRookLeft = new methods.rook({type:'R',color:'white',currentX:1,currentY:1});
+    methods.lightRookRight = new methods.rook({type:'R',color:'white',currentX:8,currentY:1});
+    methods.lightKnightLeft = new methods.knight({type:'N',color:'white',currentX:2,currentY:1});
+    methods.lightKnightRight = new methods.knight({type:'N',color:'white',currentX:7,currentY:1});
+    methods.lightPawn1 = new methods.pawn({type:'P',color:'white',currentX:1,currentY:2});
+    methods.lightPawn2 = new methods.pawn({type:'P',color:'white',currentX:2,currentY:2});
+    methods.lightPawn3 = new methods.pawn({type:'P',color:'white',currentX:3,currentY:2});
+    methods.lightPawn4 = new methods.pawn({type:'P',color:'white',currentX:4,currentY:2});
+    methods.lightPawn5 = new methods.pawn({type:'P',color:'white',currentX:5,currentY:2});
+    methods.lightPawn6 = new methods.pawn({type:'P',color:'white',currentX:6,currentY:2});
+    methods.lightPawn7 = new methods.pawn({type:'P',color:'white',currentX:7,currentY:2});
+    methods.lightPawn8 = new methods.pawn({type:'P',color:'white',currentX:8,currentY:2});
   },
   renderDark:function(){
-    methods.darkKing = new methods.king({type:'K',color:'black',currentX:'5',currentY:'8'});
-    methods.darkQueen = new methods.queen({type:'Q',color:'black',currentX:'4',currentY:'8'});
-    methods.darkBishopRight = new methods.bishop({type:'B',color:'black',currentX:'3',currentY:'8'});
-    methods.darkBishopLeft = new methods.bishop({type:'B',color:'black',currentX:'6',currentY:'8'});
-    methods.darkRookRight = new methods.rook({type:'R',color:'black',currentX:'1',currentY:'8'});
-    methods.darkRookLeft = new methods.rook({type:'R',color:'black',currentX:'8',currentY:'8'});
-    methods.darkKnightRight = new methods.knight({type:'N',color:'black',currentX:'2',currentY:'8'});
-    methods.darkKnightLeft = new methods.knight({type:'N',color:'black',currentX:'7',currentY:'8'});
+    methods.darkKing = new methods.king({type:'K',color:'black',currentX:5,currentY:8});
+    methods.darkQueen = new methods.queen({type:'Q',color:'black',currentX:4,currentY:8});
+    methods.darkBishopRight = new methods.bishop({type:'B',color:'black',currentX:3,currentY:8});
+    methods.darkBishopLeft = new methods.bishop({type:'B',color:'black',currentX:6,currentY:8});
+    methods.darkRookRight = new methods.rook({type:'R',color:'black',currentX:1,currentY:8});
+    methods.darkRookLeft = new methods.rook({type:'R',color:'black',currentX:8,currentY:8});
+    methods.darkKnightRight = new methods.knight({type:'N',color:'black',currentX:2,currentY:8});
+    methods.darkKnightLeft = new methods.knight({type:'N',color:'black',currentX:7,currentY:8});
+    methods.darkPawn1 = new methods.pawn({type:'P',color:'black',currentX:1,currentY:7});
+    methods.darkPawn2 = new methods.pawn({type:'P',color:'black',currentX:2,currentY:7});
+    methods.darkPawn3 = new methods.pawn({type:'P',color:'black',currentX:3,currentY:7});
+    methods.darkPawn4 = new methods.pawn({type:'P',color:'black',currentX:4,currentY:7});
+    methods.darkPawn5 = new methods.pawn({type:'P',color:'black',currentX:5,currentY:7});
+    methods.darkPawn6 = new methods.pawn({type:'P',color:'black',currentX:6,currentY:7});
+    methods.darkPawn7 = new methods.pawn({type:'P',color:'black',currentX:7,currentY:7});
+    methods.darkPawn8 = new methods.pawn({type:'P',color:'black',currentX:8,currentY:7});
   },
   alphaToNumeral:function(x){
     if(x.toLowerCase()==='a'){
@@ -118,7 +126,7 @@ var methods={
     $('.c'+this.currentX+'.r'+this.currentY).css({'color':spec.color});
     this.move = function(x,y){
       x = methods.alphaToNumeral(x);
-      if(Math.abs(x - this.currentX) === Math.abs(y - this.currentY) || x === parseInt(this.currentX) || y === (this.currentY)){
+      if(Math.abs(x - this.currentX) === Math.abs(y - this.currentY) || x === this.currentX || y === this.currentY){
         $('.c'+this.currentX+'.r'+this.currentY).html('');
         $('.c'+x+'.r'+y).html(spec.type);
         $('.c'+x+'.r'+y).css({'color':spec.color});
@@ -158,7 +166,7 @@ var methods={
     $('.c'+this.currentX+'.r'+this.currentY).css({'color':spec.color});
     this.move = function(x,y){
       x = methods.alphaToNumeral(x);
-      if(x === parseInt(this.currentX) || y === parseInt(this.currentY)){
+      if(x === this.currentX || y === this.currentY){
         $('.c'+this.currentX+'.r'+this.currentY).html('');
         $('.c'+x+'.r'+y).html(spec.type);
         $('.c'+x+'.r'+y).css({'color':spec.color});
@@ -198,7 +206,18 @@ var methods={
     $('.c'+this.currentX+'.r'+this.currentY).css({'color':spec.color});
     this.move = function(x,y){
       x = methods.alphaToNumeral(x);
-      if(x === parseInt(this.currentX) && y - this.currentY === 1){
+      if(this.currentY === 2 || this.currentY === 7){
+        if(x === this.currentX && (y - this.currentY === 2 || y - this.currentY === 1)){
+          $('.c'+this.currentX+'.r'+this.currentY).html('');
+          $('.c'+x+'.r'+y).html(spec.type);
+          $('.c'+x+'.r'+y).css({'color':spec.color});
+          this.currentX = x;
+          this.currentY = y;
+        }else{
+          alert('INVALID MOVE');
+          alert('NEWx='+x+'--NEWy='+y+'--OLDx='+this.currentX+'--OLDy='+this.currentY);
+        }
+      }else if(x === this.currentX && y - this.currentY === 1){
         $('.c'+this.currentX+'.r'+this.currentY).html('');
         $('.c'+x+'.r'+y).html(spec.type);
         $('.c'+x+'.r'+y).css({'color':spec.color});
